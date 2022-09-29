77538cb0ab81ab3f83a7ed52bf3b0deb7265e66d
Soumyabrata Chatterjee <61630257+Soumyabrata546@users.noreply.github.com>
2022-09-29 12:18:06 +0530

    Add files via upload

---
+++ Inhr3.java
@@ -0,0 +1,32 @@
+package PallabiMaam;
+
+
+class Vehicle3{//parent class A
+	void running() {
+		System.out.println("running mode is on");
+	}
+}
+	class Bike2 extends Vehicle3{//child class b
+		void run() {
+			System.out.println("Bike run mode on");
+		}
+	}
+	class Car2 extends Vehicle3{//child class c
+		void speed() {
+			System.out.println("Car is running");
+		}
+	}
+
+public class Inhr3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car2 c1=new Car2();//object of child class
+		c1.speed();//child class method
+		c1.running();//parent class
+		Bike2 b1=new Bike2();
+		b1.run();
+		b1.running();
+	}
+
+}
