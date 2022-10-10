e0d85047cc53452b4829abd295cd73811bdabbd5
Soumyabrata Chatterjee <61630257+Soumyabrata546@users.noreply.github.com>
2022-10-10 12:08:59 +0530

    Add files via upload

--- Moverload.java
+++ Moverload.java
@@ -8,9 +8,13 @@ static int sum(int a,int b) {
 static int sum(int a,int b,int c) {
 	return a+b+c;
 }
+static double sum(double a,double b) {
+	return a+b;
+}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println(sum(5,6));
+System.out.println(sum(7.2,6.3));
 System.out.println(sum(5,6,7));
 	}
 
