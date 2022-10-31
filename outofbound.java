784ff730ea852d1a579c88da5d0e6d58d1be6d91
Soumyabrata Chatterjee <61630257+Soumyabrata546@users.noreply.github.com>
2022-10-31 14:07:26 +0530

    Add files via upload

---
+++ outofbound.java
@@ -0,0 +1,14 @@
+package PallabiMaam;
+
+public class outofbound {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int arr[]=new int[3];
+arr[0]=2;
+arr[1]=3;
+arr[2]=5;
+arr[3]=7;// index 3 is not there in the arr size of 3
+	
+}
+}
